38c72a9e92da529b46059b8e709f5f4c9b18964f
Bob Jack <bobjack1313@gmail.com>
2026-07-09 12:25:38 -0500

    Project reorginzation for web work

---
+++ python/solver.py
@@ -0,0 +1,10 @@
+"""
+Future solver.
+
+This module will implement the search algorithm that determines:
+
+- Optimal boss offer
+- Optimal raid response
+- Maximum survivable rounds
+- Strategy evaluation
+"""
